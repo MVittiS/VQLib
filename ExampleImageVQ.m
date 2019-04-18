93f815fa389b895640be1dcfa61c92bf7f27d8d9
MVittiS <mvittis@icloud.com>
2019-04-18 06:58:25 +0100

    Added (incomplete) example script for VQ usage

---
+++ ExampleImageVQ.m
@@ -0,0 +1,36 @@
+%   This script uses the three provided functions to exemplify a typical
+%    VQ workflow: reshaping (and optionally weighting) the data,
+%    generating a dictionary, storing said dictionary and indices in some
+%    space-saving format, and then loading and decoding them again to
+%    compare against the original data.
+%    
+%   Here we load the provided example image (Mushroom.jpeg), partition
+%    it in 2x2 blocks, flatten said blocks to 4x1 vectors, make our
+%    dictionary, store the dictionary and indices as 8bpp values, and
+%    finally undo the process to compare the image against the original,
+%    in terms of error. We then re-generate the dictionary, but weighting
+%    the color values acording to the ITU-T B.709 spec for luminance.
+%    
+%   In the end, this produces an image with an approximately 12:1 fixed
+%    compression ratio, and very similar charcteristics to the texture
+%    format used by Videologic's PowerVR2 GPU, used in the Sega Dreamcast
+%    videogames console. Block artifacts are clearly visible, but having
+%    such a nice fixed compression ratio and decoding speed is a good
+%    demonstration of some of the capabilities of VQ.
+
+%% Load and convert image type
+img = single(imread('Mushroom.jpg')) / 255.0;
+%imshow(img);
+
+%% Convert image into blocks
+blockLen = 2;
+blocks = im2col(img(:, :, 1), [blockLen blockLen], 'distinct');
+blocks = [blocks; im2col(img(:, :, 2), [blockLen blockLen], 'distinct')];
+blocks = [blocks; im2col(img(:, :, 3), [blockLen blockLen], 'distinct')];
+
+%% Create VQ Dictionary and Indices
+[dict, idx] = GenVQDictMEX(blocks, 256);
+
+%% Store Dictionary and Indices to disk
+
+
